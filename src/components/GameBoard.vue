fe8c49980396ebaf70d86a74274af39b053e3585
tkilgour <thomas@kilgour.co>
2022-11-11 11:17:09 -0500

    1 file modified

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,28 +1,63 @@
 <template>
   <div class="grid grid-cols-3 grid-rows-3 bg-gray-dark gap-0.5">
-    <GameTile v-for="(tile, index) in tiles" :key="index" @click="() => handleClick(index)">
+    <GameTile v-for="(tile, index) in tiles" :key="index" :disabled="tile.length" @click="() => handleClick(index)">
       {{ tile }}
     </GameTile>
   </div>
-  <p class="pt-8 text-2xl">It's {{ playerTurn }}'s turn.</p>
+  <p class="pt-8 text-2xl">It's {{ message }}'s turn.</p>
+  <button class="text-lg px-4 py-8" @click="resetBoard">Play again!</button>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
 import GameTile from './GameTile.vue'
 
-const boardState = ref([
-  ['X', '', 'O'],
-  ['', 'O', ''],
-  ['', '', 'X'],
-])
+const boardState = ref(null)
+const resetBoard = () =>
+  (boardState.value = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ])
+resetBoard()
 const tiles = computed(() => boardState.value.flat())
 
 const playerTurn = ref(Math.random() > 0.5 ? 'X' : 'O')
+
+const winner = ref(null)
+const checkWinState = () => {
+  // check rows for all playerTurn
+  boardState.value.forEach(row => {
+    if (row.every(item === 'playerTurn')) {
+      winner.value = playerTurn.value
+    }
+  })
+
+  // check cols for all playerTurn
+  for (let i = 0; i < 3; i++) {
+    if (boardState.value.every(row => row[i] === playerTurn.value)) {
+      winner.value = playerTurn.value
+    }
+  }
+
+  // check diagonals for all playerTurn
+}
 
 const handleClick = index => {
   const row = Math.floor(index / 3)
   const col = index % 3
   boardState.value[row][col] = playerTurn.value
+
+  checkWinState()
+
+  playerTurn.value === 'X' ? (playerTurn.value = 'O') : (playerTurn.value = 'X')
 }
+
+const message = computed(() => {
+  if (winner.value) {
+    return `${winner.value} wins!`
+  }
+  // TODO: handle draw
+
+  return `It's ${playerTurn.value}'s turn.`
 </script>
